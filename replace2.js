aab7320b75a38bdd07320ebfb0b69a80882a5201
johnfr14 <jonathan.alyra@gmail.com>
2021-02-18 17:19:15 +0100

    commiteeee

--- replace2.js
+++ replace2.js
@@ -2,6 +2,6 @@ let str = "In cryptography, Alice and Bob are fictional characters commonly used
 As the use of Alice and Bob became more widespread, additional characters were added, sometimes each with a particular meaning.\
 The most common characters are Alice and Bob. Eve, Mallory, and Trent are also common names."
 
-const newStr = str.replace("Alice","Jonathan")
+const newStr = str.replaceAll("Alice","Jonathan")
 
 console.log(newStr)
